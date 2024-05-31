08ca9a6b2bc7a067a10d076f2f6d2e148554baf9
Mubashirisreal <147941230+Mubashirisreal@users.noreply.github.com>
2024-05-31 23:34:26 +0530

    Add files via upload

---
+++ day 8/2.js
@@ -0,0 +1,23 @@
+// document.getElementById("btn").addEventListener("click",()=>{
+//     let username = document.getElementById("username").value
+//     let password = document.getElementById("password").value
+
+//     const response = fetch("https://abaya-looks-backend.vercel.app/api/v1/test", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             user:username,
+//             type:username,
+//             tyep:password,
+//     } 
+// }).then(res=>{
+//     window.location.replace('./2.html')
+// }).catch
+    
+        
+
+   
+
+// })
